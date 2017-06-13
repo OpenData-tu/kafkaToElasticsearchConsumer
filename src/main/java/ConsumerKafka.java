6ade8e401a64bc578130b0efd6ef6197edfde2c6
Ahmad Jawid Jami <ahmadjawid@Ahmads-MacBook-Pro.local>
2017-06-14 00:52:17 +0200

    first commit

---
+++ src/main/java/ConsumerKafka.java
@@ -0,0 +1,91 @@
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by ahmadjawid on 6/13/17.
+ */
+
+public class ConsumerKafka {
+    private final ConsumerConnector consumer;
+    private final String topic;
+    private  ExecutorService executor;
+
+    public ConsumerKafka(String a_zookeeper, String a_groupId, String a_topic) {
+        consumer = kafka.consumer.Consumer.createJavaConsumerConnector(
+                createConsumerConfig(a_zookeeper, a_groupId));
+        this.topic = a_topic;
+    }
+
+    public void shutdown() {
+        if (consumer != null) consumer.shutdown();
+        if (executor != null) executor.shutdown();
+        try {
+            if (!executor.awaitTermination(5000, TimeUnit.MILLISECONDS)) {
+                System.out.println("Timed out waiting for consumer threads to shut down, exiting uncleanly");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted during shutdown, exiting uncleanly");
+        }
+    }
+
+    public void run(int a_numThreads) {
+        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+        topicCountMap.put(topic, new Integer(a_numThreads));
+        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
+        List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
+
+        // now launch all the threads
+        //
+        executor = Executors.newFixedThreadPool(a_numThreads);
+
+        // now create an object to consume the messages
+        //
+        int threadNumber = 0;
+        for (final KafkaStream stream : streams) {
+            executor.submit(new ConsumerTest(stream, threadNumber));
+            threadNumber++;
+        }
+    }
+
+    private static ConsumerConfig createConsumerConfig(String a_zookeeper, String a_groupId) {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", a_zookeeper);
+        props.put("group.id", a_groupId);
+        props.put("zookeeper.session.timeout.ms", "400");
+        props.put("zookeeper.sync.time.ms", "200");
+        props.put("auto.commit.interval.ms", "1000");
+
+        return new ConsumerConfig(props);
+    }
+
+    public static void main(String[] args) throws IOException {
+        String zooKeeper = "localhost:2181";
+        String groupId = "example";
+        String topic = "weatherData";
+        int threads = 1;
+
+        ConsumerKafka example = new ConsumerKafka(zooKeeper, groupId, topic);
+        example.run(threads);
+
+//        try {
+//            Thread.sleep(10000);
+//        } catch (InterruptedException ie) {
+//
+//        }
+//        example.shutdown();
+    }
+}
---
+++ src/main/java/ConsumerTest.java
@@ -0,0 +1,60 @@
+
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+
+import java.io.IOException;
+import java.util.Collections;
+
+/**
+ * Created by ahmadjawid on 6/13/17.
+ */
+
+public class ConsumerTest implements Runnable {
+    private KafkaStream m_stream;
+    private int m_threadNumber;
+
+    public ConsumerTest(KafkaStream a_stream, int a_threadNumber) {
+        m_threadNumber = a_threadNumber;
+        m_stream = a_stream;
+    }
+
+    public void run() {
+
+        RestClient restClient = RestClient.builder(
+                new HttpHost("localhost", 9200, "http")).build();
+
+        ConsumerIterator<byte[], byte[]> it = m_stream.iterator();
+
+        while (it.hasNext()){
+            HttpEntity entity = new NStringEntity(
+                    new String(it.next().message()), ContentType.APPLICATION_JSON);
+
+            try {
+                Response indexResponse = restClient.performRequest(
+                        "POST",
+                        "/weather/luften/",
+                        Collections.<String, String>emptyMap(),
+                        entity);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Thread " + m_threadNumber + ": " + new String(it.next().message()));
+        }
+
+        try {
+            restClient.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Shutting down Thread: " + m_threadNumber);
+    }
+}
